7b7e85e5bced964532827f991f29e681cfd171eb
EL FEHRI Asmae <elfehri.asmae2000gmail.com>
2024-11-22 19:24:10 +0100

    pour tester

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>  // Ajout de cette ligne
+
+int main() {
+    double x = 3.0;
+    printf("cabs(%f) = %f\n", x, cabs(x));
+    return 0;
+}
+
